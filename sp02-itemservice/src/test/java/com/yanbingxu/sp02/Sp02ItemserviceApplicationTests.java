937acee0d2f6b063521ed860f40e61f797e2ee5f
yuanzhibx_glb <ybx0729@163.com>
2020-07-28 11:13:27 +0800

    Yuanzhibx add sp02-itemservice Module

---
+++ sp02-itemservice/src/test/java/com/yanbingxu/sp02/Sp02ItemserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yanbingxu.sp02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sp02ItemserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
