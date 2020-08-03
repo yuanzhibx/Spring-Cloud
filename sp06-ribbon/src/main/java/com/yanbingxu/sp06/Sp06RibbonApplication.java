418e843a205cb16608470f07df7778fd2c7c81c0
yuanzhibx_glb <ybx0729@163.com>
2020-08-03 16:13:21 +0800

    Yuanzhibx Hystrix 降级

--- sp06-ribbon/src/main/java/com/yanbingxu/sp06/Sp06RibbonApplication.java
+++ sp06-ribbon/src/main/java/com/yanbingxu/sp06/Sp06RibbonApplication.java
@@ -2,6 +2,7 @@ package com.yanbingxu.sp06;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
@@ -12,6 +13,7 @@ import org.springframework.web.client.RestTemplate;
  * @author Yuanzhibx
  * @Date 2020-07-31
  */
+@EnableCircuitBreaker
 @EnableDiscoveryClient
 @SpringBootApplication
 public class Sp06RibbonApplication {
--- sp06-ribbon/src/main/java/com/yanbingxu/sp06/controller/RibbonController.java
+++ sp06-ribbon/src/main/java/com/yanbingxu/sp06/controller/RibbonController.java
@@ -1,5 +1,6 @@
 package com.yanbingxu.sp06.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.yanbingxu.sp01.pojo.Item;
 import com.yanbingxu.sp01.pojo.Order;
 import com.yanbingxu.sp01.pojo.User;
@@ -30,6 +31,7 @@ public class RibbonController {
      * @return
      */
     @GetMapping("/item-service/{orderId}")
+    @HystrixCommand(fallbackMethod = "getItemsFB")
     public JsonResult<List<Item>> getItems(@PathVariable String orderId) {
         return restTemplate.getForObject("http://item-service/{1}", JsonResult.class, orderId);
     }
@@ -42,6 +44,7 @@ public class RibbonController {
      * @return
      */
     @PostMapping("/item-service/decreaseNumber")
+    @HystrixCommand(fallbackMethod = "decreaseNumberFB")
     public JsonResult<?> decreaseNumber(@RequestBody List<Item> items) {
         return restTemplate.postForObject("http://item-service/decreaseNumber", items, JsonResult.class);
     }
@@ -54,6 +57,7 @@ public class RibbonController {
      * @return
      */
     @GetMapping("/user-service/{userId}")
+    @HystrixCommand(fallbackMethod = "getUserFB")
     public JsonResult<User> getUser(@PathVariable Integer userId) {
         return restTemplate.getForObject("http://user-service/{1}", JsonResult.class, userId);
     }
@@ -67,6 +71,7 @@ public class RibbonController {
      * @return
      */
     @GetMapping("/user-service/{userId}/score")
+    @HystrixCommand(fallbackMethod = "addScoreFB")
     public JsonResult<?> addScore(@PathVariable Integer userId, Integer score) {
         return restTemplate.getForObject("http://user-service/{1}/score?score={2}", JsonResult.class, userId, score);
     }
@@ -79,6 +84,7 @@ public class RibbonController {
      * @return
      */
     @GetMapping("/order-service/{orderId}")
+    @HystrixCommand(fallbackMethod = "getOrderFB")
     public JsonResult<Order> getOrder(@PathVariable String orderId) {
         return restTemplate.getForObject("http://order-service/{1}", JsonResult.class, orderId);
     }
@@ -90,8 +96,35 @@ public class RibbonController {
      * @return
      */
     @GetMapping("/order-service/")
+    @HystrixCommand(fallbackMethod = "addOrderFB")
     public JsonResult<?> addOrder() {
         return restTemplate.getForObject("http://order-service/", JsonResult.class);
+    }
+
+    // ---------------------------------
+
+    public JsonResult<List<Item>> getItemsFB(@PathVariable String orderId) {
+        return JsonResult.err().msg("获取订单的商品列表失败~~~~");
+    }
+
+    public JsonResult<?> decreaseNumberFB(@RequestBody List<Item> items) {
+        return JsonResult.err().msg("减少商品库存失败~~~");
+    }
+
+    public JsonResult<User> getUserFB(@PathVariable Integer userId) {
+        return JsonResult.err().msg("获取用户失败~~~~");
+    }
+
+    public JsonResult<?> addScoreFB(@PathVariable Integer userId, Integer score) {
+        return JsonResult.err().msg("添加用户积分失败~~~~");
+    }
+
+    public JsonResult<Order> getOrderFB(@PathVariable String orderId) {
+        return JsonResult.err().msg("获取订单失败~~~~");
+    }
+
+    public JsonResult<?> addOrderFB() {
+        return JsonResult.err().msg("添加订单失败~~~~");
     }
 
 }
