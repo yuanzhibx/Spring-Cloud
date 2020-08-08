6ec3745cadeb4142625adf03642fdbdf66387635
yuanzhibx_glb <ybx0729@163.com>
2020-08-08 16:40:40 +0800

    Yuanzhibx add rabbitmq-springboot Module

---
+++ rabbitmq-springboot/src/main/java/com/yanbingxu/RabbitmqSpringbootApplication.java
@@ -0,0 +1,17 @@
+package com.yanbingxu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yanbingxu
+ * @Date 2020-08-08
+ */
+@SpringBootApplication
+public class RabbitmqSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqSpringbootApplication.class, args);
+    }
+
+}
