271f298c956bfd02f57528f45bf9dd8301b9b1ef
yuanzhibx_glb <ybx0729@163.com>
2020-08-10 11:48:13 +0800

    Yuanzhibx sleuth 链路跟踪

--- sp02-itemservice/src/main/java/com/yanbingxu/sp02/item/controller/ItemController.java
+++ sp02-itemservice/src/main/java/com/yanbingxu/sp02/item/controller/ItemController.java
@@ -34,7 +34,7 @@ public class ItemController {
     @GetMapping("/{orderId}")
     public JsonResult<List<Item>> getItems(@PathVariable String orderId) throws InterruptedException {
         log.info("server.port=" + port + ", orderId=" + orderId);
-        double d = 0.9;
+        double d = 0.5;
         if (Math.random() < d) {
             // 90% 概率会执行延迟代码 (延迟 5 秒内随机延迟时长)
             int t = new Random().nextInt(5000);
